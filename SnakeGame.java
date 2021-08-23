a3687e5acfc489b45fcc184d1c2a05860236013e
Tiago Rodrigues <61442108+rtiago45@users.noreply.github.com>
2021-08-23 10:18:28 -0300

    Add files via upload

---
+++ SnakeGame.java
@@ -0,0 +1,11 @@
+
+public class SnakeGame {
+
+	public static void main(String[] args) {
+		
+		// não precisa criar um GameFrame frame = new GameFrame(), já que não será utilizado cria somente o new GameFrame()
+		new GameFrame();
+
+	}
+
+}
